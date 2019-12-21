03a2cd9da73b4ebe71d3086bd453cfb4f7497d0d
Sebastian <monika.lukowska@gmail.com>
2019-12-21 15:51:36 +0100

    j

--- startup/db.js
+++ startup/db.js
@@ -5,6 +5,10 @@ const config = require('config')
 
 module.exports = function() {
   const db = config.get('db');
-  mongoose.connect(db)
+  mongoose.connect(db, {
+    useCreateIndex: true,
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+  })
   .then(() => winston.info(`Connected to ${db}..`))
 }

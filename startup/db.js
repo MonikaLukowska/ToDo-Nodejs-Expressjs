8d2d8f40c8ad7c0bb72d7f6114b5c39b39f7339f
Sebastian <monika.lukowska@gmail.com>
2019-12-21 18:19:57 +0100

    test

--- startup/db.js
+++ startup/db.js
@@ -5,7 +5,7 @@ const config = require('config')
 
 module.exports = function() {
   const db = config.get('db');
-  mongoose.connect(db)
+  mongoose.connect('mongodb://127.0.0.1:27017/test')
   .then(() => winston.info(`Connected to ${db}..`))
   .catch(err => {
     winston.info(`DB Connection Error: ${err.message}`)});

50a705e3f7b69d68f3c5f93b88020aa1c82041d3
Sebastian <monika.lukowska@gmail.com>
2019-12-21 15:02:36 +0100

    another one

--- startup/logging.js
+++ startup/logging.js
@@ -14,7 +14,7 @@ module.exports = function() {
       })
       
       winston.add(winston.transports.File, {filename: 'logfile.log'});
-     // winston.add(winston.transports.MongoDB, {db: 'mongodb://localhost/ToDoList'});
+     winston.add(winston.transports.MongoDB, {db: 'ToDoList_db'});
       
     
       
